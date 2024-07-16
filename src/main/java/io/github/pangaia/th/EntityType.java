4dc5a8d89e09d84dbb17216e3e5209047ffd0fac
Roman Bortnikow <rb2o2.dev@gmail.com>
2024-07-16 18:44:17 +0300

    jumps

---
+++ src/main/java/io/github/pangaia/th/EntityType.java
@@ -0,0 +1,7 @@
+package io.github.pangaia.th;
+
+public enum EntityType {
+    PLAYER,
+    PLATFORM,
+    BOSS
+}
--- src/main/java/io/github/pangaia/th/ToxicHunterApplication.java
+++ src/main/java/io/github/pangaia/th/ToxicHunterApplication.java
@@ -4,11 +4,18 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.physics.CollisionHandler;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class ToxicHunterApplication extends GameApplication {
     private Entity player;
+    private Entity boss;
+    private Entity platform0;
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -28,8 +35,59 @@ public class ToxicHunterApplication extends GameApplication {
         int maxY = FXGL.getAppHeight();
 
         player = FXGL.entityBuilder()
-                .at(maxX/2.0,maxY*2.0/3.0)
-                .view(new Rectangle(maxX/25.0, maxY/10.0, Color.INDIANRED))
+                .type(EntityType.PLAYER)
+                .at(maxX / 2.0, maxY * 2.0 / 3.0)
+                .viewWithBBox(new Rectangle(maxX / 25.0, maxY / 10.0, Color.INDIANRED))
+                .with(new CollidableComponent(true))
                 .buildAndAttach();
+        player.setProperty("vspeed", 0.0);
+        player.setProperty("stands", true);
+        boss = FXGL.entityBuilder()
+                .type(EntityType.BOSS)
+                .at(maxX * 1.0 / 25.0, maxY * (2.0 / 3.0 - 0.2 + 0.1))
+                .view(new Rectangle(maxX / 10.0, maxY / 5.0, Color.BLUE))
+                .buildAndAttach();
+        platform0 = FXGL.entityBuilder()
+                .type(EntityType.PLATFORM)
+                .at(0, maxY * (2.0 / 3.0 + 0.1))
+                .viewWithBBox(new Rectangle(maxX, maxY * 0.05, Color.SADDLEBROWN))
+                .with(new CollidableComponent(true))
+                .buildAndAttach();
+    }
+
+    @Override
+    protected void onUpdate(double tpf) {
+        double speed = player.getDouble("vspeed");
+        if (!player.getBoolean("stands")) {
+            player.translateY(speed /2.0);
+            player.setProperty("vspeed", speed + tpf*100.0);
+        }
+    }
+
+    @Override
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.PLATFORM) {
+            @Override
+            protected void onCollisionBegin(Entity player, Entity floor) {
+                player.setProperty("vspeed", 0.0);
+                player.setProperty("stands", Boolean.TRUE);
+            }
+        });
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = FXGL.getInput();
+
+        input.addAction(new UserAction("Jump") {
+            @Override
+            protected void onAction() {
+                if (player.getBoolean("stands")) {
+                    player.translateY(0.0);
+                    player.setProperty("stands", false);
+                    player.setProperty("vspeed", -25.0);
+                }
+            }
+        }, KeyCode.SPACE);
     }
 }
